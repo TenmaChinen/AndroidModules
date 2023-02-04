f112b0569e374948db46f4f85ec9b3e49f8ddb58
TenmaMune <tenmachinen@gmail.com>
2023-02-04 18:29:37 +0100

    Set to minimal dependencies without google MaterialComponents

--- app/src/main/java/com/softramen/modules/fadeRecyclerView/FadeRecyclerViewActivity.java
+++ app/src/main/java/com/softramen/modules/fadeRecyclerView/FadeRecyclerViewActivity.java
@@ -2,29 +2,26 @@ package com.softramen.modules.fadeRecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.softramen.modules.R;
 import com.softramen.recyclerviews.FadeRecyclerView;
 
 public class FadeRecyclerViewActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate( final Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_fade_recycler_view );
+	@Override
+	protected void onCreate( final Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_fade_recycler_view );
 
-        final Context context = this;
+		final Context context = this;
 
-        final String[] stringItems = { "A" , "B" , "C" , "D" , "E" };
-        final FadeRecyclerView recyclerView = findViewById( R.id.fade_recyler_view );
-        final RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter( stringItems );
-        final RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( context );
+		final String[] stringItems = { "A" , "B" , "C" , "D" , "E" };
+		final FadeRecyclerView recyclerView = findViewById( R.id.fade_recyler_view );
+		final RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter( stringItems );
+		final FadeRecyclerView.LayoutManager layoutManager = new LinearLayoutManager( context );
 
-        recyclerView.setLayoutManager( layoutManager );
-        recyclerView.setAdapter( recyclerViewAdapter );
-    }
+		recyclerView.setLayoutManager( layoutManager );
+		recyclerView.setAdapter( recyclerViewAdapter );
+	}
 }
